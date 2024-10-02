94cd6f947299ecdff40d384566f672a3fbf91306
ericksilva232323 <erick14silva14@gmail.com>
2024-10-02 11:20:37 -0300

    up

--- api/prisma/seed.js
+++ api/prisma/seed.js
@@ -1,4 +1,25 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-const clientes = require("")
+const clientes = require("./dados/clientes.json")
+const automoveis = require("./dados/automoveis.json")
+const alocacao = require("./dados/alocacao.json")
+const concessionaria = require("./dados/concessionaria.json")
+
+async function main() {
+    for (const locacao of locacao) {
+        await prisma.locacao.create({
+            data: locacao
+        });
+    }
+    for (const clientes of clientes) {
+        await prisma.clientes.create({
+            data: clientes
+        });
+    }
+    for (const alocacao of alocacao){
+        await prisma.alocacao.create({
+            data: alocacao
+        })
+    }
+}
